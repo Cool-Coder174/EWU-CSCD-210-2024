460ac4f15d8212de218d10f1c005a10b0c50eee4
Isaac <Isaachernandez174@gmail.com>
2024-01-17 12:47:28 -0800

    added Lab2

---
+++ cscd210/Lab2/src/cscd210lab2/CSCD210Lab2.java
@@ -0,0 +1,39 @@
+package cscd210lab2;
+
+
+import java.util.Scanner;
+import java.util.DecimalFormat;
+import java.util.HashMap;
+import java.util.hashmap;
+
+public class CSCD210Lab2 {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+        
+    }
+
+    static void Speed(int finishTime, int distance){
+
+        // Vars for calculations
+        int meterPerSec = 0;
+        int feetPerSec = 0;
+        int milesPerHour = 0;
+        int kilometersPerHour = 0;
+        int mileRun = 0;
+        int yardRun = 0;
+
+
+
+        // Vars for output
+        HashMap<Integer, String> speedMap = new HashMap<Integer, String>();
+
+
+        
+
+
+
+    }
+
+
+}
