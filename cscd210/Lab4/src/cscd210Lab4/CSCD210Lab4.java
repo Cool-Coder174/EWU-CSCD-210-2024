d101b24352e0e003fa33d3c11ec3cae0aac1f26c
Isaac <Isaachernandez174@gmail.com>
2024-02-02 21:29:15 -0800

    starter code added

---
+++ cscd210/Lab4/src/cscd210Lab4/CSCD210Lab4.java
@@ -0,0 +1,25 @@
+package cscd210lab4;
+import java.util.Scanner;
+public class CSCD210Lab4
+{
+// Must specify constant names for residential base fee
+// and business base fee
+public static void main(String [] args)
+{
+int beginningMeterValue, endingMeterValue;
+char customerCode;
+Scanner kb = new Scanner(System.in);
+System.out.print("Please enter the beginning meter value ");
+beginningMeterValue = kb.nextInt();
+/*
+Your code goes below
+Steps in getting to the solution
+1) Prompt for ending meter value
+2) Read in the ending meter value
+3) Prompt for the customer code
+4) Read in the customer code.
+5) Calculate bill
+6) Display results
+*/
+}// end main
+}// end class
